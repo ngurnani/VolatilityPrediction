ab0acb4d8aaeed2705503676d0fffb986e54e853
ngurnani <ngurnani@gmail.com>
2018-05-20 01:42:10 -0700

    implementing simple novas transform -- currently incomplete

--- src/NoVaS.py
+++ src/NoVaS.py
@@ -12,8 +12,11 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from scipy.stats import kurtosis
 
-def simple_novas(x):
-	pass
+def simple_novas(returns, p):
+	a = (np.ones(p+1))/(p+1)
+	squared_returns = returns**2
+	denominator = math.sqrt(np.dot(a, squared_returns.tail(p+1)[::-1]))	
+	return np.divide(sp500_returns, denominator)
 
 def exponential_novas(x):
 	pass
