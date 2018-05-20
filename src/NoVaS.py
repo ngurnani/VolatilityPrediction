fec11535daee22fb7c0251791be531111eb465c2
ngurnani <ngurnani@gmail.com>
2018-05-19 20:03:55 -0700

    confirmed scipystats kurtosis function is same as that from the paper, deleted extra emprical_kurtosis() function from novas.py

--- src/NoVaS.py
+++ src/NoVaS.py
@@ -9,16 +9,8 @@ Last Updated: 5/19/2018
 
 import numpy as np
 import pandas as pd
-import math 
-
-
-def empirical_kurtosis(x):
-	""" Calculates empirical kurtosis of input array of data """
-	n = len(x)
-	numerator = (sum(np.subtract(x,np.mean(x)))**4)/n
-	denominator = ((sum(np.subtract(x,np.mean(x)))**2)/n)**2
-	return numerator/denominator
-
+import matplotlib.pyplot as plt
+from scipy.stats import kurtosis
 
 def simple_novas(x):
 	pass
