f4913bd85f3f3476441f0919d244528336a4a19e
ngurnani <ngurnani@gmail.com>
2018-05-26 10:53:20 -0700

    initialized garch.py module

---
+++ src/garch.py
@@ -0,0 +1,18 @@
+"""
+Contains implementation of the GARCH(1,1) model for volatility prediction
+
+Author: Nishant D. Gurnani
+Date Created: 5/26/2018
+Last Updated: 5/26/2018
+"""
+
+
+def garch11_loglikelihood():
+	pass
+
+def garch11():
+	pass
+
+def prediction_interval():
+	pass
+
