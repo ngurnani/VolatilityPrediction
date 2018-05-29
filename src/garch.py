b7d880c8b84817e8d033fd3131eda42b6c14df0b
ngurnani <ngurnani@gmail.com>
2018-05-29 00:11:01 -0700

    file clean up, removed obsolete info

--- src/garch.py
+++ src/garch.py
@@ -19,70 +19,71 @@ import matplotlib.pyplot as plt
 
 ######### SELF GARCH IMPLEMENTATION #####################
 
-def compute_squared_sigmas(X, initial_sigma, theta):
-	"""Function to compute the sigmas given the initial guess"""
+# def compute_squared_sigmas(X, initial_sigma, theta):
+# 	"""Function to compute the sigmas given the initial guess"""
 
-    a0 = theta[0]
-    a1 = theta[1]
-    b1 = theta[2]
+#     a0 = theta[0]
+#     a1 = theta[1]
+#     b1 = theta[2]
     
-    T = len(X)
-    sigma2 = np.ndarray(T)
+#     T = len(X)
+#     sigma2 = np.ndarray(T)
     
-    sigma2[0] = initial_sigma ** 2
+#     sigma2[0] = initial_sigma ** 2
     
-    for t in range(1, T):
-        # Here's where we apply the equation
-        sigma2[t] = a0 + a1 * X[t-1]**2 + b1 * sigma2[t-1]
+#     for t in range(1, T):
+#         # Here's where we apply the equation
+#         sigma2[t] = a0 + a1 * X[t-1]**2 + b1 * sigma2[t-1]
     
-    return sigma2
+#     return sigma2
 
-def negative_log_likelihood(X, theta):
-    """takes as input returns X and model parameters theta and returns the negative
-       log likelihood"""
+# def negative_log_likelihood(X, theta):
+#     """takes as input returns X and model parameters theta and returns the negative
+#        log likelihood"""
 
-    T = len(X)
+#     T = len(X)
     
-    # Estimate initial sigma squared
-    initial_sigma = np.sqrt(np.mean(X ** 2))
+#     # Estimate initial sigma squared
+#     initial_sigma = np.sqrt(np.mean(X ** 2))
     
-    # Generate the squared sigma values
-    sigma2 = compute_squared_sigmas(X, initial_sigma, theta)
+#     # Generate the squared sigma values
+#     sigma2 = compute_squared_sigmas(X, initial_sigma, theta)
     
-    # Now actually compute
-    return -sum(
-        [-np.log(np.sqrt(2.0 * np.pi)) -
-        (X[t] ** 2) / (2.0 * sigma2[t]) -
-        0.5 * np.log(sigma2[t]) for
-         t in range(T)]
-    )
+#     # Now actually compute
+#     return -sum(
+#         [-np.log(np.sqrt(2.0 * np.pi)) -
+#         (X[t] ** 2) / (2.0 * sigma2[t]) -
+#         0.5 * np.log(sigma2[t]) for
+#          t in range(T)]
+#     )
 
-def garch11(X,theta):
-	objective = partial(negative_log_likelihood, X)
+# def garch11(X,theta):
+# 	objective = partial(negative_log_likelihood, X)
 
-	# Define the constraints for our minimizer
-	def constraint1(theta):
-	    return np.array([1 - (theta[1] + theta[2])])
+# 	# Define the constraints for our minimizer
+# 	def constraint1(theta):
+# 	    return np.array([1 - (theta[1] + theta[2])])
 
-	def constraint2(theta):
-	    return np.array([theta[1]])
+# 	def constraint2(theta):
+# 	    return np.array([theta[1]])
 
-	def constraint3(theta):
-	    return np.array([theta[2]])
+# 	def constraint3(theta):
+# 	    return np.array([theta[2]])
 
-	cons = ({'type': 'ineq', 'fun': constraint1},
-	        {'type': 'ineq', 'fun': constraint2},
-	        {'type': 'ineq', 'fun': constraint3})
+# 	cons = ({'type': 'ineq', 'fun': constraint1},
+# 	        {'type': 'ineq', 'fun': constraint2},
+# 	        {'type': 'ineq', 'fun': constraint3})
 
-	# Actually do the minimization
-	result = scipy.optimize.minimize(objective, (1, 0.5, 0.5),
-	                        method='SLSQP',
-	                        constraints = cons)
+# 	# Actually do the minimization
+# 	result = scipy.optimize.minimize(objective, (1, 0.5, 0.5),
+# 	                        method='SLSQP',
+# 	                        constraints = cons)
 
-	theta_mle = result.x
-	return theta_mle
+# 	theta_mle = result.x
+# 	return theta_mle
 
 ############## L1 GARCH PREDICTION #############
+
 def arch_p(C, A, B, returns, p):
     """returns sigma^2 term in ARCH(p) model using coefficients from fitted GARCH(1,1) model """
 
